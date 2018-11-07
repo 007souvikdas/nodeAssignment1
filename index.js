b334c43e6e18cfadb5c4a08f8d80af60d0dffa47
Souvik das <dashedsouvik@gmail.com>
2018-11-07 16:04:36 +0530

    Assignment #1
    Please create a simple "Hello World" API. Meaning:
    1. It should be a RESTful JSON API that listens on a port of your choice. 
    2. When someone posts anything to the route /hello, you should return a welcome message, in JSON format. This message can be anything you want.

---
+++ index.js
@@ -0,0 +1,34 @@
+﻿//Hello world RESTFul API
+var http = require('http');
+var url = require('url');
+var config = {
+    httpPort: 2001
+};
+var httpServer = http.createServer((req, res) => {
+    var path = url.parse(req.url);
+    var incomingPath = path.pathname.replace(/^\/+|\/+$/g, '');
+    var methodToBeCalled = route[incomingPath] ? route[incomingPath] : methodHandler.NotFound;
+
+    methodToBeCalled((statusCode, msg) => {
+
+        var returnCode = (typeof (stausCode) == 'number') ? statusCode : 404;
+        var message = (typeof (msg) == 'object') ? msg : { 'message': 'Page Not Found' };
+        res.writeHead(returnCode, { 'content-Type': 'application/json' });
+        res.end(JSON.stringify(message));
+    });
+});
+httpServer.listen(config.httpPort, () => {
+    console.log(`started listening on port:${config.httpPort}`);
+});
+
+var methodHandler = {};
+methodHandler.hello = function (callback) {
+    callback(200, { 'message': 'Hello World' });
+}
+methodHandler.NotFound = function (callback) {
+    callback();
+}
+
+var route = {
+    'hello': methodHandler.hello
+};
